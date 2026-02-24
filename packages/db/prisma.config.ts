eb277b6f672a8319b39d1aeb08560a1e5e7d48de
Thomas <thomasmetias29@gmail.com>
2026-02-24 23:46:42 +0200

    feat(prisma): create solid prisma foundation
    - init prisma and connect to supabase db
    - prisma foundation is according to latest
    prisam v7.4 and latest turborepo docs

---
+++ packages/db/prisma.config.ts
@@ -0,0 +1,12 @@
+import { config } from "dotenv";
+import { defineConfig, env } from "@prisma/config";
+import path from "node:path";
+
+config({ path: path.resolve(process.cwd(), "../../.env") });
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DIRECT_URL") ?? env("DATABASE_URL"),
+  },
+});
---
+++ packages/db/src/index.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import { PrismaClient } from "./generated/client/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+// 1. Create a standard Postgres connection pool
+const pool = new Pool({ connectionString });
+
+// 2. Wrap it in the Prisma adapter
+const adapter = new PrismaPg(pool);
+
+// 3. Singleton pattern to prevent connection limits in development
+const globalForPrisma = globalThis as typeof globalThis & { prisma?: PrismaClient };
+
+export const db = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
+
+export * from "./generated/client/client";
