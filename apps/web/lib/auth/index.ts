aab8d326e1e7e1eb6d0ab9c2771e659a15010f57
Thomas <thomasmetias29@gmail.com>
2026-05-05 22:52:22 +0300

    feat(sign-in): add sign-in logic

--- apps/web/lib/auth/index.ts
+++ apps/web/lib/auth/index.ts
@@ -1,4 +1,4 @@
-import { createAuthClient } from "../supabase/supabase-client"
+import { createAuthClient } from '../supabase/supabase-client'
 
 export const getInitialSession = async () => {
   const supabase = createAuthClient()
@@ -9,5 +9,36 @@ export const getInitialSession = async () => {
     return data.session
   } catch (error) {
     console.error('Error at AuthContext/getInitialState: ', error)
+  }
+}
+
+export const signIn = async (email: string, password: string) => {
+  const supabase = createAuthClient()
+  if (supabase === undefined)
+    throw new Error('Error at signIn: Supabase client not initialized')
+
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    if (error) {
+      return {
+        success: false,
+        error: error.message,
+      }
+    }
+
+    return {
+      success: true,
+      data,
+    }
+  } catch (error: any) {
+    console.error('Error at AuthContext/signIn: ', error)
+    return {
+      success: false,
+      error: error.message,
+    }
   }
 }
